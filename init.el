eae9436e58b7050be48e6a82f5fffbc1c3898299
Mike Zamansky <zamansky@gmail.com>
2014-10-19 14:19:09 -0400

    Z

---
+++ init.el
@@ -0,0 +1,15 @@
+(package-initialize nil)
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/") t)
+
+;; Override the packages with the git version of Org and other packages
+(package-initialize t)
+(setq package-enable-at-startup nil)
+(require 'org)
+(require 'ob-tangle)
+(load "~/.emacs.secrets" t)
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
