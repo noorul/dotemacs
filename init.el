c1950d2e198ec4853f4db6e361cb3e7003cffb21
Mike Zamansky <zamansky@gmail.com>
2015-03-25 09:55:11 -0400

    Z

--- init.el
+++ init.el
@@ -24,10 +24,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-	 (quote
-		("b71d5d49d0b9611c0afce5c6237aacab4f1775b74e513d8ba36ab67dfab35e5a" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
- '(inhibit-startup-screen t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
  '(org-agenda-files (quote ("~/btsync/orgfiles/")))
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/btsync/orgfiles")
@@ -37,14 +36,13 @@
  '(org-mobile-inbox-for-pull "~/btsync/orgfiles/MobileOrg/pull.org")
  '(org-startup-folded (quote content))
  '(org-startup-indented t)
- '(paradox-github-token t)
- '(quack-programs
+ '(package-selected-packages
 	 (quote
-		("mzscheme" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
+		(edit-server 4clojure clojure-cheatsheet ac-cider cider clojure-mode cdnjs tern-auto-complete tern ac-js2 js2-refactor json-mode js2-mode web-mode ac-html ghc hi2 flycheck-haskell ac-haskell-process virtualenvwrapper jedi processing-snippets processing-mode auto-complete smartparens flycheck-pyflakes flycheck helm-c-yasnippet java-snippets yasnippet helm-projectile projectile gist magit rainbow-delimiters aggressive-indent gnuplot ox-reveal org-gcal ob-mongo visual-regexp all expand-region multiple-cursors browse-kill-ring ace-isearch ace-window ace-jump-zap ace-jump-mode undo-tree zenburn-theme color-theme smart-mode-line-powerline-theme guide-key hydra helm-swoop helm hungry-delete use-package))))
 (put 'narrow-to-region 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
