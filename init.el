05c388c51719fa2d797185285d6bec19cd57590e
Mike Zamansky <zamansky@gmail.com>
2015-04-05 20:43:45 -0400

    Z

--- init.el
+++ init.el
@@ -24,9 +24,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
  '(org-agenda-files (quote ("~/btsync/orgfiles/")))
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/btsync/orgfiles")
@@ -38,7 +35,7 @@
  '(org-startup-indented t)
  '(package-selected-packages
 	 (quote
-		(bug-hunter edit-server 4clojure clojure-cheatsheet ac-cider cider clojure-mode cdnjs tern-auto-complete tern ac-js2 js2-refactor json-mode js2-mode web-mode ac-html ghc hi2 flycheck-haskell ac-haskell-process virtualenvwrapper jedi processing-snippets processing-mode auto-complete smartparens flycheck-pyflakes flycheck helm-c-yasnippet java-snippets yasnippet helm-projectile projectile gist magit rainbow-delimiters aggressive-indent gnuplot ox-reveal org-gcal ob-mongo visual-regexp all expand-region multiple-cursors browse-kill-ring ace-isearch ace-window ace-jump-zap ace-jump-mode undo-tree zenburn-theme color-theme smart-mode-line-powerline-theme guide-key hydra helm-swoop helm hungry-delete use-package))))
+		(ghc hi2 flycheck-haskell ac-haskell-process zenburn-theme web-mode visual-regexp virtualenvwrapper use-package undo-tree tern-auto-complete smartparens smart-mode-line-powerline-theme restclient rainbow-delimiters processing-snippets processing-mode paradox ox-reveal org-gcal ob-mongo ob-http nyan-prompt nyan-mode magit json-mode jedi java-snippets idomenu hydra hungry-delete helm-projectile helm-c-yasnippet guide-key gnuplot gist fold-dwim flycheck-pyflakes expand-region engine-mode elpy edit-server discover-my-major discover-js2-refactor diff-hl color-theme clojure-cheatsheet cdnjs browse-kill-ring all aggressive-indent ace-window ace-jump-zap ace-isearch ac-php ac-js2 ac-html ac-cider 4clojure))))
 (put 'narrow-to-region 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
