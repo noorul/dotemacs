8acce06e968c77caa00a5adbf29c33bc753358a7
Mike Zamansky <zamansky@gmail.com>
2015-10-11 10:36:22 -0400

    Started to simplify config

--- init.el
+++ init.el
@@ -18,3 +18,24 @@
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/btsync/orgfiles/")))
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/btsync/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote content))
+ '(org-startup-indented t)
+ '(package-selected-packages
+	 (quote
+		(jedi auto-complete edit-server flycheck-pyflakes flycheck rainbow-delimiters aggressive-indent ox-reveal gnuplot ob-mongo visual-regexp browse-kill-ring ace-window avy swiper undo-tree zenburn-theme color-theme smart-mode-line-powerline-theme guide-key hydra hungry-delete use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
