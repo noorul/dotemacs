b9a478ef059f3c57e1dada579700aee2ac4be135
Mike Zamansky <zamansky@gmail.com>
2015-10-26 16:54:35 -0400

    Z

--- init.el
+++ init.el
@@ -23,19 +23,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/btsync/orgfiles/")))
- '(org-default-notes-file (concat org-directory "/notes.org"))
- '(org-directory "~/btsync/orgfiles")
- '(org-export-html-postamble nil)
- '(org-hide-leading-stars t)
- '(org-startup-folded (quote content))
- '(org-startup-indented t)
  '(package-selected-packages
-	 (quote
-		(tern tern-auto-complete nodejs-repl ac-cider cider magit markdown-mode projectile js2-mode web-mode yasnippet yasnippets edit-server jedi flycheck-pyflakes flycheck rainbow-delimiters aggressive-indent auto-complete ox-reveal gnuplot ob-mongo visual-regexp browse-kill-ring ace-window avy swiper undo-tree zenburn-theme color-theme smart-mode-line-powerline-theme guide-key hydra hungry-delete use-package))))
+   (quote
+    (tex zenburn-theme yasnippet web-mode visual-regexp use-package undo-tree tern-auto-complete swiper smartparens smart-mode-line-powerline-theme rainbow-delimiters projectile ox-reveal ob-mongo markdown-mode magit js2-mode jedi hydra hungry-delete guide-key gnuplot flycheck-pyflakes expand-region edit-server color-theme browse-kill-ring auctex aggressive-indent ace-window ac-cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ )
