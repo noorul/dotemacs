865898e7374e89efc1e836434cd8f648622724a5
Mike Zamansky <zamansky@gmail.com>
2015-10-27 07:36:23 -0400

    Z

--- init.el
+++ init.el
@@ -18,22 +18,5 @@
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (tex zenburn-theme yasnippet web-mode visual-regexp use-package undo-tree tern-auto-complete swiper smartparens smart-mode-line-powerline-theme rainbow-delimiters projectile ox-reveal ob-mongo markdown-mode magit js2-mode jedi hydra hungry-delete guide-key gnuplot flycheck-pyflakes expand-region edit-server color-theme browse-kill-ring auctex aggressive-indent ace-window ac-cider))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-<<<<<<< HEAD
- )
-=======
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-(put 'narrow-to-region 'disabled nil)
->>>>>>> adc98eba109c2851161e9bb38e084b41aeea0e6e
+
+ 
