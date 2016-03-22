8ac78617d735a7ed34ad612676183a0d0e8a1249
Mike Zamansky <zamansky@gmail.com>
2016-03-22 18:43:33 -0400

    Modified appointment capture template to be deadline instead of scheduled

--- init.el
+++ init.el
@@ -28,9 +28,15 @@
  '(custom-safe-themes
 	 (quote
 		("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "f024aea709fb96583cf4ced924139ac60ddca48d25c23a9d1cd657a2cf1e4728" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/cloud/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
  '(package-selected-packages
 	 (quote
-		(twittering-mode circe mu4e-alert semi org-alert org autopair paredit-everywhere paredit org-present windsize try org-ac on-screen iedit volatile-highlights evil evil-surround evil-matchit highlight-parentheses rainbow-mode org-pandoc ox-pandoc ox-rst spacemacs spaceline spacemacs-theme solarized-theme ox-twbs fireplace cdnjs 4clojure clojure-snippets org-bullets zenburn-theme yasnippet which-key web-mode visual-regexp use-package undo-tree tern-auto-complete swiper smartparens smart-mode-line-powerline-theme rainbow-delimiters projectile ox-reveal ob-mongo markdown-mode magit live-py-mode js2-mode jedi hydra hungry-delete guide-key gnuplot flycheck-pyflakes expand-region edit-server color-theme browse-kill-ring beacon auctex aggressive-indent ace-window ac-cider))))
+		(dired+ twittering-mode circe mu4e-alert semi org-alert org autopair paredit-everywhere paredit org-present windsize try org-ac on-screen iedit volatile-highlights evil evil-surround evil-matchit highlight-parentheses rainbow-mode org-pandoc ox-pandoc ox-rst spacemacs spaceline spacemacs-theme solarized-theme ox-twbs fireplace cdnjs 4clojure clojure-snippets org-bullets zenburn-theme yasnippet which-key web-mode visual-regexp use-package undo-tree tern-auto-complete swiper smartparens smart-mode-line-powerline-theme rainbow-delimiters projectile ox-reveal ob-mongo markdown-mode magit live-py-mode js2-mode jedi hydra hungry-delete guide-key gnuplot flycheck-pyflakes expand-region edit-server color-theme browse-kill-ring beacon auctex aggressive-indent ace-window ac-cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
